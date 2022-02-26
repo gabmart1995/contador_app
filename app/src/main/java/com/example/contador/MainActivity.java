fe6992d5a8f49e7aff1683d48f44c4322eeee26b
gabmart1995 <gabmart1995@gmail.com>
2022-02-26 18:03:59 -0400

    wip: se agrega validacion con numeros negativos

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -3,37 +3,66 @@ package com.example.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int counter = 0;
 
+    TextView textResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // se inicializa los elementos del xml
+        textResult = ( TextView ) findViewById( R.id.contador_pulsaciones );
     }
 
     public void incrementCounter( View view ) {
         counter++;
-        showResult();
+        textResult.setText( "" + counter );
     }
 
     public void decrementCounter( View view ) {
+
+        CheckBox checkNegative = ( CheckBox ) findViewById( R.id.negtives );
+
         if ( counter > 0 ) {
             counter--;
-            showResult();
+
+        } else {
+
+            if ( !checkNegative.isChecked() ) {
+                counter = 0;
+
+            } else  {
+                counter--;
+
+            }
         }
+
+        textResult.setText( "" + counter );
     }
 
     public void resetCounter( View view ) {
-        counter = 0;
-        showResult();
-    }
 
-    public void showResult() {
-        TextView textResult = ( TextView ) findViewById( R.id.contador_pulsaciones );
+        EditText textNumber = ( EditText ) findViewById( R.id.n_reset );
+
+        try {
+            // getText obtiene un Objeto Editable a String con toString
+            counter = Integer.parseInt( textNumber.getText().toString() );
+
+        } catch ( Exception error ) {
+            counter = 0;
+
+        }
+
+        // limpiamos el campo de texto
+        textNumber.setText( "" );
         textResult.setText( "" + counter );
     }
 }
